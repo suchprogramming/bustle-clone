a26fd6960fb6ecfff6c757af7932b2d8eef868a6
Andy Uppendahl and Tyler Whitsett <a.uppendahl@gmail.com>
2015-03-02 10:56:43 -0800

    Add routes and links for all three newly added templates.

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 Bustle.Router.map(function() {
   this.resource('headlines', {path: '/'});
   this.resource('1-Cords-Are-A-Health-Hazard');
+  this.resource('2-I-Know-Nothing-About-Fashion');
+  this.resource('3-Trouble-In-Middle-East');
+  this.resource('4-Yellow-Shirt-People');
 });
